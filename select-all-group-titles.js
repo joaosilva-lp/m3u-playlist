03d17b2d87c7ef1812a1a61a4235d795dee664a0
João Silva <99473819+joaosilva-lp@users.noreply.github.com>
2023-02-06 07:53:01 +0000

    Create select-all-group-titles.js

---
+++ select-all-group-titles.js
@@ -0,0 +1,48 @@
+ const fs = require('fs');
+
+fs.readFile('group-titles.txt', 'utf-8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+
+  const groupTitles = data.split('\n').map(title => `group-title="${title}"`);
+
+  fs.readdir('./', (err, files) => {
+    if (err) {
+      throw err;
+    }
+
+    const m3uFiles = files.filter(file => file.endsWith('.m3u'));
+
+    m3uFiles.forEach(file => {
+      fs.readFile(`./${file}`, 'utf-8', (err, data) => {
+        if (err) {
+          throw err;
+        }
+
+        const lines = data.split('\n');
+        const items = [];
+
+        for (let i = 0; i < lines.length; i++) {
+          const line = lines[i];
+          if (line.startsWith('#EXTINF:') && groupTitles.some(title => line.includes(title))) {
+            const nextLine = lines[i + 1];
+            items.push(line + '\n' + nextLine);
+			console.log(line + '\n' + nextLine);
+          }
+        }
+
+        items.forEach(item => {
+          console.log(item);
+        });
+
+        fs.writeFile(`output-${file}`, items.join('\n'), (writeErr) => {
+          if (writeErr) {
+            throw writeErr;
+          }
+          console.log(`Saved items to output-${file}`);
+        });
+      });
+    });
+  });
+});
